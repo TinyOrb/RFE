3be9eb8f69bacc75120f62b27e23c96d2b55fcca
shad hasan <baksh12shad@gmail.com>
2019-06-27 23:45:38 +0530

    some progress

--- App1/robot_runner/invoke.py
+++ App1/robot_runner/invoke.py
@@ -1,12 +1,10 @@
 import subprocess
-import os
-import sys
 import json
 import datetime
-
-from RFE.App1.Robot_loader import Al_robot
-from RFE.App1.Robot_loader import Al_robot_parser
-
+import time
+import os
+import threading
+import RFE.App1.Robot_loader.Al_robot as Al_robot
 import RFE.App1.settings as meta
 
 result_dir = None
@@ -53,6 +51,8 @@ def trigger(feature, suite=None, tc=None, variable=None, variablefile=None, incl
         current_state["cmd"] += "-t \"%s\" " % tc
         current_state["cmd"] += os.path.join(Al_robot.fetch_All_suite()[feature], suite)
 
+        update_current(current_state, feature, suite, tc)
+
     elif suite is not None:
         current_state = fetch_current(feature, suite, tc)
         if len(current_state) != 0:
@@ -78,6 +78,7 @@ def trigger(feature, suite=None, tc=None, variable=None, variablefile=None, incl
             current_state["cmd"] += "--variablefile==\"%s\" " % variablefile
         current_state["cmd"] += "--outputdir \"%s\" " % current_state["dir"]
         current_state["cmd"] += os.path.join(Al_robot.fetch_All_suite()[feature], suite)
+        update_current(current_state, feature, suite, tc)
 
     elif suite is not None:
         current_state = fetch_current(feature, suite, tc)
@@ -104,16 +105,16 @@ def trigger(feature, suite=None, tc=None, variable=None, variablefile=None, incl
             current_state["cmd"] += "--variablefile==\"%s\" " % variablefile
         current_state["cmd"] += "--outputdir \"%s\" " % current_state["dir"]
         current_state["cmd"] += Al_robot.fetch_All_suite()[feature]
+        update_current(current_state, feature, suite, tc)
 
     else:
         print("No input provided")
+        return
 
-def create_track(feature, suite=None, tc=None):
-    pass
-
-
-def create_output_path(feature, suite=None, tc=None):
-    pass
+    print("Main    : before creating thread")
+    x = threading.Thread(target=run, args=(current_state, feature, suite, tc,))
+    print("Main    : before running thread")
+    x.start()
 
 
 def get_run_state(feature, suite=None, tc=None):
@@ -121,9 +122,11 @@ def get_run_state(feature, suite=None, tc=None):
 
 
 def fetch_history(feature, suite=None, tc=None):
+    print("Fetching query")
     with open('track.json') as json_file:
         data = json.load(json_file)
     flag = 0
+    status = None
     for data_feature in data["features"]:
         if data_feature["name"] == feature:
             if suite is not None:
@@ -133,21 +136,28 @@ def fetch_history(feature, suite=None, tc=None):
                             for data_tc in data_suite["tcs"]:
                                 if data_tc["name"] == tc:
                                     flag = 1
-                                    return data_tc["history_status"]
+                                    status = data_tc["history_status"]
                         else:
                             flag = 2
-                            return data_suite["history_status"]
+                            status = data_suite["history_status"]
             else:
                 flag = 3
-                return data_feature["history_status"]
+                status = data_feature["history_status"]
     if flag == 0:
         print("query failed")
+    elif flag == 1 or flag == 2 or flag == 3:
+        print("data fetch successfully")
+    else:
+        print("unknown query")
+    return status
 
 
 def fetch_current(feature, suite=None, tc=None):
+    print("Fetching query")
     with open('track.json') as json_file:
         data = json.load(json_file)
     flag = 0
+    status = None
     for data_feature in data["features"]:
         if data_feature["name"] == feature:
             if suite is not None:
@@ -157,18 +167,24 @@ def fetch_current(feature, suite=None, tc=None):
                             for data_tc in data_suite["tcs"]:
                                 if data_tc["name"] == tc:
                                     flag = 1
-                                    return data_tc["current_status"]
+                                    status = data_tc["current_status"]
                         else:
                             flag = 2
-                            return data_suite["current_status"]
+                            status = data_suite["current_status"]
             else:
                 flag = 3
-                return data_feature["current_status"]
+                status = data_feature["current_status"]
     if flag == 0:
         print("query failed")
+    elif flag == 1 or flag == 2 or flag == 3:
+        print("data fetch successfully")
+    else:
+        print("unknown query")
+    return status
 
 
 def update_history(current_status, feature, suite=None, tc=None):
+    print("Updating status")
     with open('track.json') as json_file:
         data = json.load(json_file)
 
@@ -192,14 +208,23 @@ def update_history(current_status, feature, suite=None, tc=None):
                 flag = 3
 
     if flag == 0:
-        print("Insertion success")
-
-    with open('track.json', 'w') as outfile:
-        json.dump(data, outfile)
+        print("Insertion failed")
+        return False
+    elif flag == 1 or flag == 2 or flag == 3:
+        try:
+            with open('track.json', 'w') as outfile:
+                json.dump(data, outfile)
+            print("Insertion success")
+            return True
+        except:
+            print("Unknown exception")
+    else:
+        print("Unknown state")
+        return False
 
 
 def update_current(current_status, feature, suite=None, tc=None):
-
+    print("Updating status")
     with open('track.json') as json_file:
         data = json.load(json_file)
     flag = 0
@@ -221,10 +246,19 @@ def update_current(current_status, feature, suite=None, tc=None):
                 flag = 3
 
     if flag == 0:
-        print("Insertion success")
-
-    with open('track.json', 'w') as outfile:
-        json.dump(data, outfile)
+        print("Insertion failed")
+        return False
+    elif flag == 1 or flag == 2 or flag == 3:
+        try:
+            with open('track.json', 'w') as outfile:
+                json.dump(data, outfile)
+            print("Insertion success")
+            return True
+        except:
+            print("Unknown exception")
+    else:
+        print("Unknown state")
+        return False
 
 
 def get_tc():
@@ -239,16 +273,45 @@ def get_feature():
     pass
 
 
-def run(cmd):
-    k = os.system(cmd)
+def run(current_state, feature, suite, tc):
+    if not ensure_path(current_state["script_output"], type="file"):
+        print("Failed to create script output file")
+        return False
+    subprocess.Popen(["echo ******start of script output****** > %s ; %s >> %s ; "
+                      "echo ******end of script output****** >> %s " %
+                      (current_state["script_output"], current_state["cmd"], current_state["script_output"], current_state["script_output"])],
+                     stdout=subprocess.PIPE, shell=True)
+
+    current_state["status"] = "running"
+    update_current(current_state, feature, suite, tc)
+
+    line = subprocess.check_output(['tail', '-1', current_state["script_output"]])
+
+    while "******end of script output******" not in line:
+        time.sleep(5)
+
+    current_state["status"] = "done"
+    cur_time = str(datetime.datetime.now())
+    cur_time1 = cur_time.split(" ")[0] + "_" + cur_time.split(" ")[1]
+    current_state["end_time"] = cur_time1
+    update_current(current_state, feature, suite, tc)
+
+    print("Thread ended")
+    return True
 
 
-def ensure_path(path):
+def ensure_path(path, type="dir"):
     try:
-        if not os.path.exists(path):
-            os.makedirs(path)
-        else:
-            print("Directory all ready exist")
+        if type == "dir":
+            if not os.path.exists(path):
+                os.makedirs(path)
+            else:
+                print("Directory all ready exist")
+        elif type == "file":
+            if not os.path.exists(os.path.dirname(path)):
+                os.makedirs(os.path.dirname(path))
+            else:
+                print("Directory all ready exist")
         return True
     except Exception as e:
         print("Some unknown error", str(e))
@@ -256,23 +319,19 @@ def ensure_path(path):
 
 
 def fetch_detail_traverse_data(data, feature, suite, tc):
-    detail = {"feature":None, "suite":None, "tc":None}
+    detail = {"feature": None, "suite": None, "tc": None}
 
     try:
         for data_feature in data["features"]:
             if feature == data_feature["name"]:
                 detail["feature"] = feature
 
-                if suite is None:
-                    return data_feature
-                else:
+                if suite is not None:
                     for data_suite in data_feature["suites"]:
                         if suite == data_suite["name"]:
                             detail["suite"] = suite
 
-                            if tc is None:
-                                return data_suite
-                            else:
+                            if tc is not None:
                                 for data_tc in data_suite["tcs"]:
                                     if tc == data_tc["name"]:
                                         detail["tc"] = tc
@@ -283,11 +342,11 @@ def fetch_detail_traverse_data(data, feature, suite, tc):
 
 
 def ensure_data_set(data, feature, suite, tc):
-
+    global result_dir
     current_data_set = fetch_detail_traverse_data(data, feature, suite, tc)
 
-    if current_data_set["feature"] is not None:
-        if current_data_set["suite"] is not None:
+    if current_data_set["feature"] is not None and suite is None:
+        if current_data_set["suite"] is not None and tc is None:
             if current_data_set["tc"] is not None:
                 print("Data set already exist")
             else:
@@ -379,4 +438,8 @@ def insert_data_set(data, feature, suite, FEATURE=None, SUITE=None, TC=None):
 if __name__ == "__main__":
     p_feature = "project1"
     p_suite = "mtu.robot"
+
+    #p_tc = "TC_101"
+    #trigger(feature=p_feature, suite=p_suite, tc=p_tc)
+
     trigger(feature=p_feature, suite=p_suite)
